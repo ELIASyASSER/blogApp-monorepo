80bf259b6b6e0c07de2b9ce7f7dead4021e59ab5
elias <eliasyasser193@gmail.com>
2024-10-07 15:08:27 +0300

    improving the site experience and fix some errors

--- frontend/blogapp/src/layout/PostPage.js
+++ frontend/blogapp/src/layout/PostPage.js
@@ -20,7 +20,7 @@ function PostPage() {
   }
 
   return (
-    <div className='max-w-4xl mx-auto my-8 p-4 bg-[#f8f8f9] rounded-lg shadow-lg'>
+    <div className='max-w-5xl  mx-auto my-8 p-4 bg-[#f8f8f9] rounded-lg shadow-lg '>
       <div className='img mb-4'>
         <img
           className='w-full h-64 object-cover rounded-lg'
@@ -28,7 +28,7 @@ function PostPage() {
           alt={postInfo.title}
         />
       </div>
-      <h1 className='text-3xl font-bold mb-4'>{postInfo.title}</h1>
+      <h1 className='text-3xl font-bold mb-4 w-[88%] break-words'>{postInfo.title}</h1>
       <div className='flex items-center space-x-4 text-gray-600 mb-6'>
         <time className='text-sm'>{new Date(postInfo.createdAt).toDateString()}</time>
         <span className='text-sm'>|</span>
